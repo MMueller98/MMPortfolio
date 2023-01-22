61b54f0802b4ad73cec39ecd8df9376fb14781e8
MMueller98 <82333133+MMueller98@users.noreply.github.com>
2023-01-22 19:00:50 +0100

    restructure project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Col, Container, Row, Table } from "react-bootstrap";
-import koesterlich_img from "../assets/Koesterlich_Kollage.png";
+import { Col, Container, Row } from "react-bootstrap";
+import koesterlich_img from "../assets/proj_koesterlich.png";
+import mmportfolio_img from "../assets/proj_mmportfolio.png"
 import "../styles/Projects.css";
 
 
@@ -11,27 +12,29 @@ function Projects() {
                 <h1 className="mb-5">Projects</h1>
             </div>
 
+            {/** KOESTERLICH APP */}
             <Row>
                 <Col xs={12} md={5} className="image-col">
-                    <img src={koesterlich_img} alt="Marius" />
+                    <img src={koesterlich_img} alt="Koesterlich App" />
                 </Col>
                 <Col xs={12} md={7} className="description-col ps-5 pe-5">
                     <h2 className="mb-3">Koesterlich - Android App</h2>
-                    <p>In 2021 I successfully completed my studies in Biomedical Engineering at the Trier University of Applied Sciences. In the course of my studies I discovered a new love for computer science and therefore started a second degree in computer science B. Sc.</p>
-                    <p>I am currently working as a working student at the BBT Group in the field of Applications – Clinical Special Systems.</p>
+                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
                 </Col>
             </Row>
 
             <span className="line-long mt-5 mb-5"></span>
 
+            {/** MM-PORTFOLIO */}
             <Row>
                 <Col xs={12} md={7} className="description-col ps-5 pe-5">
-                    <h2 className="mb-3">Koesterlich - Android App</h2>
-                    <p>In 2021 I successfully completed my studies in Biomedical Engineering at the Trier University of Applied Sciences. In the course of my studies I discovered a new love for computer science and therefore started a second degree in computer science B. Sc.</p>
-                    <p>I am currently working as a working student at the BBT Group in the field of Applications – Clinical Special Systems.</p>
+                    <h2 className="mb-3">MM-Portfolio</h2>
+                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
                 </Col>
                 <Col xs={12} md={5} className="image-col">
-                    <img src={koesterlich_img} alt="Marius" />
+                    <img src={mmportfolio_img} alt="MM-Portfolio" />
                 </Col>
             </Row>
 
